c8879f7d82e8630ec157e5066d2200cd8681e333
foldi.soma.vajk <foldi.soma.vajk@BURG-2E05B-13>
2024-09-10 13:59:12 +0200

    Első commit

---
+++ foci/dolgok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foci
+{
+    internal class dolgok
+    {
+        public dolgok(int fordulo, int hazaigolok, int vendeggolok, int hazaifelidogolok, int vendegfelidogolok, string hazaicsapatnev, string vendegcsapatnev)
+        {
+            Fordulo = fordulo;
+            Hazaigolok = hazaigolok;
+            Vendeggolok = vendeggolok;
+            Hazaifelidogolok = hazaifelidogolok;
+            Vendegfelidogolok = vendegfelidogolok;
+            Hazaicsapatnev = hazaicsapatnev;
+            Vendegcsapatnev = vendegcsapatnev;
+        }
+
+        public int Fordulo { get; set; }
+        public int Hazaigolok { get; set; }
+        public int Vendeggolok { get; set; }
+        public int Hazaifelidogolok { get; set; }
+        public int Vendegfelidogolok { get; set; }
+        public string Hazaicsapatnev {  get; set; }
+        public string Vendegcsapatnev { get; set; }
+    }
+}
